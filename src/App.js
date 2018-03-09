6acf601edb1b250b9c544b2cc3d13c6e63f1778b
Patrick Bacon-Blaber <pablaber225@gmail.com>
2018-03-08 19:16:44 -0500

    add date time to top, reformat layout

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 
 import NywContainer from './NywContainer';
 import MtaContainer from './MtaContainer';
+import DateTime from './DateTime';
 
 class App extends Component {
   render() {
     return (
       <div className="app-container">
-        <NywContainer />
-        <MtaContainer />
+        <div className="darken"></div>
+        <div className="content">
+          <div className="top">
+            <DateTime />
+          </div>
+          <div className="filler"></div>
+          <div className="bottom">
+            <NywContainer />
+            <MtaContainer />
+          </div>
+        </div>
+
       </div>
     );
   }
